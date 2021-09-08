474ff15d6201d4f3787372d2c2843c0bcc5fab3c
491859828 <491859828@qq.com>
2021-09-08 11:36:23 +0800

    ok

--- server.js
+++ server.js
@@ -19,24 +19,31 @@ var server = http.createServer(function(request, response){
 
   /******** 从这里开始看，上面不要看 ************/
 
-  // console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
+  console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
 
-  // if(path === '/'){
-  //   response.statusCode = 200
-  //   response.setHeader('Content-Type', 'text/html;charset=utf-8')
-  //   response.write(`二哈`)
-  //   response.end()
-  // } else if(path === '/x'){
-  //   response.statusCode = 200
-  //   response.setHeader('Content-Type', 'text/css;charset=utf-8')
-  //   response.write(`body{color: red;}`)
-  //   response.end()
-  // } else {
-  //   response.statusCode = 404
-  //   response.setHeader('Content-Type', 'text/html;charset=utf-8')
-  //   response.write(`你输入的路径不存在对应的内容`)
-  //   response.end()
-  // }
+  if(path === '/'){
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write(`
+    <h1>呵呵<h1/>
+    <link rel="stylesheet" href="/x">
+    `)
+    response.end()
+  } else if(path === '/x'){
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/css;charset=utf-8')
+    response.write(`
+    h1{
+      color:red;
+    }
+    `)
+    response.end()
+  } else {
+    response.statusCode = 404
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write(`你访问的页面不存在`)
+    response.end()
+  }
   if(path==='/goin'){
     console.log("a person goin");
     response.end("hello,person");
